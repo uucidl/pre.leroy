684b80ae7243af90b723cb05bf23520105b5c240
nicolas <nicolas@uucidl.com>
2017-07-21 09:50:47 +0200

    Add mu

---
+++ proposals/xxxx_mu.h
@@ -0,0 +1,161 @@
+/*
+ * @url: https://gist.github.com/pervognsen/6a67966c5dc4247a0021b95c8d0a7b72
+ * @lang: c99
+ * @taglist: platform
+ * @dependencylist: xxxx_mu_win32
+ *
+ * A streamlined platform layer for audio/visual interactive programs.
+ *
+ * API style is push/pull; coroutine; client/server type.
+ */
+
+#include <stdint.h>
+
+enum {
+    MU_FALSE = 0,
+    MU_TRUE = 1,
+    MU_MAX_KEYS = 256,
+    MU_MAX_TEXT = 256,
+    MU_MAX_ERROR = 1024,
+    MU_CTRL = 0x11, // VK_CONTROL
+    MU_ALT = 0x12, // VK_MENU
+    MU_SHIFT = 0x10, // VK_SHIFT
+    MU_MAX_AUDIO_BUFFER = 2 * 1024
+};
+
+typedef uint8_t Mu_Bool;
+
+struct Mu_Int2 {
+    int x;
+    int y;
+};
+
+struct Mu_DigitalButton {
+    Mu_Bool down;
+    Mu_Bool pressed;
+    Mu_Bool released;
+};
+
+struct Mu_AnalogButton {
+    float threshold;
+    float value;
+    Mu_Bool down;
+    Mu_Bool pressed;
+    Mu_Bool released;
+};
+
+struct Mu_Stick {
+    float threshold;
+    float x;
+    float y;
+};
+
+struct Mu_Gamepad {
+    Mu_Bool connected;
+    struct Mu_DigitalButton a_button;
+    struct Mu_DigitalButton b_button;
+    struct Mu_DigitalButton x_button;
+    struct Mu_DigitalButton y_button;
+    struct Mu_AnalogButton left_trigger;
+    struct Mu_AnalogButton right_trigger;
+    struct Mu_DigitalButton left_shoulder_button;
+    struct Mu_DigitalButton right_shoulder_button;
+    struct Mu_DigitalButton up_button;
+    struct Mu_DigitalButton down_button;
+    struct Mu_DigitalButton left_button;
+    struct Mu_DigitalButton right_button;
+    struct Mu_Stick left_thumb_stick;
+    struct Mu_Stick right_thumb_stick;
+    struct Mu_DigitalButton left_thumb_button;
+    struct Mu_DigitalButton right_thumb_button;
+    struct Mu_DigitalButton back_button;
+    struct Mu_DigitalButton start_button;
+};
+
+struct Mu_Mouse {
+    struct Mu_DigitalButton left_button;
+    struct Mu_DigitalButton right_button;
+    int wheel;
+    int delta_wheel;
+    struct Mu_Int2 position;
+    struct Mu_Int2 delta_position;
+};
+
+struct Mu_Window {
+    char *title;
+    struct Mu_Int2 position;
+    struct Mu_Int2 size;
+    Mu_Bool resized;
+};
+
+struct Mu_AudioFormat {
+    uint32_t samples_per_second;
+    uint32_t channels;
+    uint32_t bytes_per_sample;
+};
+
+struct Mu_AudioBuffer {
+    int16_t *samples;
+    size_t samples_count;
+    struct Mu_AudioFormat format;
+};
+
+typedef void(*Mu_AudioCallback)(struct Mu_AudioBuffer *buffer);
+
+struct Mu_Audio {
+    struct Mu_AudioFormat format;
+    Mu_AudioCallback callback;
+};
+
+struct Mu_Time {
+    uint64_t delta_ticks;
+    uint64_t delta_nanoseconds;
+    uint64_t delta_microseconds;
+    uint64_t delta_milliseconds;
+    float delta_seconds;
+
+    uint64_t ticks;
+    uint64_t nanoseconds;
+    uint64_t microseconds;
+    uint64_t milliseconds;
+    float seconds;
+
+    uint64_t initial_ticks;
+    uint64_t ticks_per_second;
+};
+
+struct Mu_Win32;
+
+struct Mu {
+    Mu_Bool initialized;
+    Mu_Bool quit;
+
+    char *error;
+    char error_buffer[MU_MAX_ERROR];
+
+    struct Mu_Window window;
+    struct Mu_DigitalButton keys[MU_MAX_KEYS];
+    struct Mu_Gamepad gamepad;
+    struct Mu_Mouse mouse;
+
+    char text[MU_MAX_TEXT];
+    size_t text_length;
+
+    struct Mu_Time time;
+    struct Mu_Audio audio;
+    struct Mu_Win32* win32;
+};
+
+Mu_Bool Mu_Initialize(struct Mu *mu);
+Mu_Bool Mu_Pull(struct Mu *mu);
+void Mu_Push(struct Mu *mu);
+
+struct Mu_Image {
+    uint8_t *pixels;
+    uint32_t channels;
+    uint32_t width;
+    uint32_t height;
+};
+
+Mu_Bool Mu_LoadImage(const char *filename, struct Mu_Image *image);
+Mu_Bool Mu_LoadAudio(const char *filename, struct Mu_AudioBuffer *audio);
